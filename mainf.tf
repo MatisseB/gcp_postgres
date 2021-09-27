0a5adf7d5b2ecfee51759190cae78f1ba9da8827
Matisse <mb.workadress@gmail.com>
2021-09-27 23:20:33 +0200

    Overriding module default vars - For better module illustration

--- mainf.tf
+++ mainf.tf
@@ -8,7 +8,8 @@ module "gcp_postgres" {
     # Variables to pass to module - Add if needed
     # Project, Region and Zone are set by default via Google provider
     environment = var.environment
-
     
-    # add some default variables
+    postgres_version = "POSTGRES_12"
+    backup_start_time = "04:00"
+    password_length = 16
 }
